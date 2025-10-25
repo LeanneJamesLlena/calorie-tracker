563c32ef21bf7fb92d4984e2dd5283517634c85e
Leanne13430 <Leanne.Llena@student.lut.fi>
2025-10-25 17:01:31 +0300

    Profile controller created

--- server/src/controllers/profile.controller.js
+++ server/src/controllers/profile.controller.js
@@ -1,13 +1,25 @@
+//define the logics of the routes inside profile.routes.js
+import { getTargets, updateTargets } from '../services/profile.service.js';
 
-
-
-
-
-export const testGetProfileRoute = async (req, res) => {
+//get the macros of the user and send it to the client
+export async function getMyTargets(req, res) {
     try {
-        res.status(200).json({message: "profile route working :)"});
+        //verifyAccess function which verify the access token will attach the user object into the req thats why we get the id from there and not from the body, all routes will be protected meaning will go through -> verifyAccess 
+        const data = await getTargets(req.user.id);
+        res.json(data);
     } catch (error) {
-        res.status(500).json({message: error.message});
-        console.log(error.message);
+        console.error(error.message);
+        res.status(400).json({error: error.message});
+    }
+}
+//put=update the Targets(Macros) of the user
+export async function putMyTargets(req, res) {
+    try {
+        // if req.body is defined by client we use it so use the left side req.body, if not return an empty object which is the right side, thats what this is for "req.body ?? {}". Its like an ternary operator ? "value" : "anothervalue"
+        const updatedData = await updateTargets(req.user.id, req.body ?? {});
+        res.json(updatedData);
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).json({error: error.message});
     }
 }
--- server/src/routes/profile.routes.js
+++ server/src/routes/profile.routes.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { verifyAccess } from '../'
 import { testGetProfileRoute }from '../controllers/profile.controller.js'
 const router = express.Router();
 
