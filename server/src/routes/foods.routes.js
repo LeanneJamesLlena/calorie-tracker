e302c91a39bb2f515cd14fec1b20851e2f5f1829
Leanne13430 <Leanne.Llena@student.lut.fi>
2025-10-27 16:01:32 +0200

    foods routes added

--- server/src/routes/foods.routes.js
+++ server/src/routes/foods.routes.js
@@ -1,10 +1,14 @@
-import express from 'express'
-import { testGetFoodRoute }from '../controllers/foods.controller.js'
+import express from 'express';
+import rateLimit from 'express-rate-limit';
+import { searchFoods, getFoodById } from '../controllers/foods.controller.js';
+
 const router = express.Router();
 
+// Lighter rate limit specifically for search endpoints
+// this will limit the clients request
+const searchLimiter = rateLimit({ windowMs: 60 * 1000, max: 30 });
 
-router.get('/', testGetFoodRoute);
-
-
+router.get('/search', searchLimiter, searchFoods);
+router.get('/:fdcId', getFoodById);
 
 export default router;
