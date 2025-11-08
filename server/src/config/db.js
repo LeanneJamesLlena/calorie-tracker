eb27d97c05eec8321670c81d5020b0ea080791f2
Leanne13430 <Leanne.Llena@student.lut.fi>
2025-11-08 15:35:21 +0200

    Commenting in db.js file fixed

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,9 +3,10 @@ import { config } from './env.js'
 /*
 CODE FOR EMPTYING database collections such as foodcache and diary entries:
 import { FoodCache } from '../models/FoodCache.model.js';
-import { DiaryEntry } from '../models/DiaryEntry.model.js;
-*/
 import { DiaryEntry } from '../models/DiaryEntry.model.js';
+import { User } from '../models/User.model.js';
+*/
+
 
 export const connectDB = async () => {
     try {
@@ -20,9 +21,15 @@ export const connectDB = async () => {
         /* CODE FOR EMPTYING diaryentries collection:
         if (process.env.CLEAR_DIARY_ENTRIES === 'true') {
             const { deletedCount } = await DiaryEntry.deleteMany({});
-            console.log(`[FoodCache] Cleared ${deletedCount} docs`);
+            console.log(`[Diary entires] Cleared ${deletedCount} docs`);
+        }
+
+        if (process.env.CLEAR_USERS === 'true') {
+            const { deletedCount } = await User.deleteMany({});
+            console.log(`[Users] Cleared ${deletedCount} docs`);
         }
         */
+
         
         console.log(`backend server connected to DATABASE!`)
     } catch (error) {
